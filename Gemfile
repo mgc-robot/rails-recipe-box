d592b8d97e4163312b0c4fcf8c8e4947aec11108
mark <markgeraldcabatingan@gmail.com>
2017-07-08 22:54:53 +0800

    add basic functionality for recipe box

--- Gemfile
+++ Gemfile
@@ -53,3 +53,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml', '~> 5.0', '>= 5.0.1'
 gem 'mysql2', '~> 0.4.7'
 gem 'simple_form', '~> 3.5'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@recipes = Recipe.all.order("created_at DESC")
 	end
 
 	def show
@@ -14,8 +15,26 @@ class RecipesController < ApplicationController
 	def create
 		@recipe = Recipe.new(recipe_params)
 		if @recipe.save
+			redirect_to @recipe, notice: "Successfully saved"
 		else
+			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @recipe.update(recipe_params)
+			redirect_to @recipe, notice: "Successfully updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@recipe.destroy
+		redirect_to root_path, notice: "Successfully deleted"
 	end
 
 	private
