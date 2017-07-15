07e591382eab4d9cc330fb16a86b9c224e5f60c0
mark <markgeraldcabatingan@gmail.com>
2017-07-15 15:34:07 +0800

    fix issue on creating new recipe with steps and direction

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-	has_many :ingredients, inverse_of: :recipe
-	has_many :directions, inverse_of: :recipe
+	has_many :ingredients, inverse_of: :recipe, dependent: :destroy
+	has_many :directions, inverse_of: :recipe, dependent: :destroy
 
 	accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
 	accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['step'].blank? }, allow_destroy: true
